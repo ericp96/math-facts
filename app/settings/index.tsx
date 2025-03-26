eddabeb168b25dff736f45871b97801dd83e24a6
Eric Pastuer <eric@pastuer.me>
2025-03-25 23:32:33 -0600

    User config header

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,33 +1,38 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from "react-native";
 
-import { View } from '../../components/library/Themed';
-import { useQuery } from '@realm/react';
-import { UserConfig } from '../../models/UserConfigModel';
-import { router } from 'expo-router';
-import { MonoText } from '../../components/library/StyledText';
+import { View } from "../../components/library/Themed";
+import { useQuery } from "@realm/react";
+import { UserConfig } from "../../models/UserConfigModel";
+import { router } from "expo-router";
+import { MonoText } from "../../components/library/StyledText";
 
 export default function SettingsMenuScreen() {
   const [userConfig] = useQuery(UserConfig);
 
   return (
     <View style={styles.container}>
+      <View style={styles.profileSection}>
+        <MonoText lightColor="#000" darkColor="#000">
+          {userConfig?.name}
+        </MonoText>
+        <MonoText lightColor="#000" darkColor="#000">
+          Add Profile
+        </MonoText>
+      </View>
       <TouchableOpacity
         onPress={() => {
-          router.push('/settings/profile');
+          router.push("/settings/profile");
         }}
-        style={styles.userInfoWrapper}
+        style={styles.button}
       >
         <MonoText lightColor="#000" darkColor="#000">
-          Name
-        </MonoText>
-        <MonoText style={styles.userInfoName} lightColor="#000" darkColor="#000">
-          {userConfig?.name}
+          Profile
         </MonoText>
       </TouchableOpacity>
 
       <TouchableOpacity
         onPress={() => {
-          router.push('/settings/addition');
+          router.push("/settings/addition");
         }}
         style={styles.button}
       >
@@ -38,7 +43,7 @@ export default function SettingsMenuScreen() {
 
       <TouchableOpacity
         onPress={() => {
-          router.push('/settings/subtraction');
+          router.push("/settings/subtraction");
         }}
         style={styles.button}
       >
@@ -49,7 +54,7 @@ export default function SettingsMenuScreen() {
 
       <TouchableOpacity
         onPress={() => {
-          router.push('/settings/multiplication');
+          router.push("/settings/multiplication");
         }}
         style={styles.button}
       >
@@ -60,7 +65,7 @@ export default function SettingsMenuScreen() {
 
       <TouchableOpacity
         onPress={() => {
-          router.push('/settings/division');
+          router.push("/settings/division");
         }}
         style={styles.button}
       >
@@ -79,6 +84,17 @@ export default function SettingsMenuScreen() {
           Custom
         </MonoText>
       </TouchableOpacity> */}
+
+      <TouchableOpacity
+        onPress={() => {
+          router.push("https://venmo.com/u/Eric-Pastuer");
+        }}
+        style={styles.linkButton}
+      >
+        <MonoText lightColor="#000" darkColor="#000">
+          Like the app? Send me a tip!
+        </MonoText>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -88,36 +104,38 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
   },
+  profileSection: {
+    marginHorizontal: 10,
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    paddingHorizontal: 5,
+  },
   label: {
     margin: 10,
     marginBottom: 0,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   textBox: {
     borderWidth: 1,
-    borderColor: '#eee',
+    borderColor: "#eee",
     padding: 10,
     margin: 10,
-  },
-  userInfoWrapper: {
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-    borderColor: '#b2cdf7',
-  },
-  userInfoName: {
-    fontSize: 24,
-    fontWeight: 'bold',
   },
   button: {
     padding: 15,
     margin: 10,
     borderWidth: 1,
-    borderColor: '#855797',
+    borderColor: "#855797",
     borderRadius: 10,
-    backgroundColor: '#b2cdf7',
-    alignItems: 'center',
+    backgroundColor: "#b2cdf7",
+    alignItems: "center",
+  },
+  linkButton: {
+    margin: 5,
+    alignContent: "center",
+    textAlign: "center",
+    alignItems: "center",
   },
 });
